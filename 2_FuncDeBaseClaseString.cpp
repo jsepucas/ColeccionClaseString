963e254a0418353cf46d8061a07169de232f5d9d
jsepucas <jsepucas@myuax.com>
2023-11-23 12:27:16 +0100

    3

--- 2_FuncDeBaseClaseString.cpp
+++ 2_FuncDeBaseClaseString.cpp
@@ -1,4 +1,19 @@
-#include <iostrean>
-# include <string>
+#include <iostream>
+#include <string>
 
-int main()
+int main() {
+    std::string cadena;
+    std::cout << "Pon una oracion: ";
+    std::getline(std::cin, cadena);
+
+    std::cout << "La cadena es: " << cadena << std::endl;
+
+    std::string cadena = "Esto es un ejemplo de cadena ";
+    std::cout <<"El tamano de la cadena es: " <<cadena.size() << std::endl;
+    if (cadena.empty()) {
+        std::cout << "La cadena esta vacia" << std::endl;
+    } else {
+        std::cout << "La cadena no esta vacia" << std::endl;
+    }
+    return 0;
+}
