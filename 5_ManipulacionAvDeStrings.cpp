8cfde7926d542418e1c7650177b0b3c6689d8e17
jsepucas <jsepucas@myuax.com>
2023-11-23 12:52:32 +0100

    4.4

--- 5_ManipulacionAvDeStrings.cpp
+++ 5_ManipulacionAvDeStrings.cpp
@@ -5,6 +5,11 @@ int main() {
     std::string cadenaPrincipal, cadenaAInsertar;
     int posicionInicio, longitud;
 
-    // Pedir al usuario que introduzca la cadena principal
     std::cout << "Introduce la cadena principal: ";
     std::getline(std::cin, cadenaPrincipal);
+
+    std::cout << "Introduce la posición de inicio para la subcadena: ";
+    std::cin >> posicionInicio;
+    std::cout << "Introduce la longitud de la subcadena: ";
+    std::cin >> longitud;
+
