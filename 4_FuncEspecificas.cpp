61b026fe0afa609cbeb3fdd83e8f27eacf6d37bd
jsepucas <jsepucas@myuax.com>
2023-11-23 12:47:08 +0100

    4.1

--- 4_FuncEspecificas.cpp
+++ 4_FuncEspecificas.cpp
@@ -7,3 +7,16 @@ int main() {
     // Pedir al usuario que introduzca la cadena principal
     std::cout << "Introduce la cadena principal: ";
     std::getline(std::cin, cadenaPrincipal);
+
+    std::cout << "Introduce la subcadena a buscar: ";
+    std::getline(std::cin, subcadenaABuscar);
+
+    // Buscar la subcadena en la cadena principal
+    size_t posicion = cadenaPrincipal.find(subcadenaABuscar);
+
+    if (posicion != std::string::npos) {
+        std::cout << "Subcadena encontrada en el índice: " << posicion << std::endl;
+
+        // Pedir al usuario que introduzca la subcadena de reemplazo
+        std::cout << "Introduce la subcadena de reemplazo: ";
+        std::getline(std::cin, subcadenaReemplazo);
